d3bac9ed9951ff063cc987197d78c35454098a2b
terry <limtaesu@gmail.com>
2018-11-01 22:14:02 -0400

    .

--- paytocomment/app.py
+++ paytocomment/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 app.config.from_object('paytocomment.config.ProductionConfig')
+app.secret_key = app.config.get('SECRET_KEY')
 
 
 @app.route('/')
 def hello_world():
-    print(app.config)
+    print(app.config.get('DATABASE_URI'))
     return render_template('index.html')
